50d28b979130e8b87cfb46e4d442005e2f618ca7
vikaspal9131 <vikaspal968562@gmail.com>
2025-03-26 18:39:15 +0530

    add vulnerability check code feature

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { openAndCompareFile } from './utils/compareFile';
 import { evaluateCode } from './utils/evaluateCode';
+import { analyzeCode }  from './utils/Vulnerability'
 
 export function activateMain() {
     // openAndCompareFile();
     evaluateCode();
+    analyzeCode();
+
 }
